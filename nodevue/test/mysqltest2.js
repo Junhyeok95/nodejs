5ba6f5e2c25881b8e58cc9b8f0edf07f95d4b869
JangJunHyeok <btrya23@gmail.com>
2020-01-29 20:59:28 +0900

    node vue, mysql db end

--- nodevue/test/mysqltest2.js
+++ nodevue/test/mysqltest2.js
@@ -10,13 +10,41 @@ const sql2 = "SELECT COUNT(*) FROM user";
 const pool = new Pool();
 
 Promise.using(pool.connect(), conn => {
-    conn.queryAsync(sql1, (err, ret) => {
-        util.log("sql1 = ", ret);
-        // util.log("sql1 = ", ret.affectedRows);
-        
-        // conn.queryAsync(sql2, (err2, ret2) => {
-        //     util.log("sql2 = ", ret.affectedRows);
-        // });
+    Promise.all([
+        conn.queryAsync(sql1),
+        conn.queryAsync(sql2)
+    ]).then(result => {
+        console.log("End Then !");
+        util.log("sql1 =", result[0]);
+        util.log("sql2 =", result[1]);
+        pool.end();
+    }).catch(err => {
+        util.log("err >>> ", err);
+        pool.end();
     });
-    pool.end();
 });
+
+
+// Promise.using(pool.connect(), conn => {
+//     conn.queryAsync(sql1)
+//         .then(console.log)
+//          .then(pool.end())
+//         .catch(err => {
+//             util.log("err >>> ", err);
+//         });
+
+//     // pool.end();
+// });
+
+
+// Promise.using(pool.connect(), conn => {
+//     conn.queryAsync(sql1, (err, ret) => {
+//         util.log("sql1 = ", ret);
+//         // util.log("sql1 = ", ret.affectedRows);
+        
+//         // conn.queryAsync(sql2, (err2, ret2) => {
+//         //     util.log("sql2 = ", ret.affectedRows);
+//         // });
+//     });
+//     pool.end();
+// });
