2afea7b90e510f47ecc0c000cab9aa465649e527
JangJunHyeok <btrya23@gmail.com>
2020-02-07 22:36:59 +0900

    node vue, App.vue / data() / methods / computed

--- nodevue/vue/hello/src/App.vue
+++ nodevue/vue/hello/src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
+    <hr>
+    <h1>{{test}} :: <span v-if="isVisible">{{visit}}</span> </h1>
+    <input type="text" v-on:keyup="visit++" v-model="test">
+    <button v-on:click="visitCount()">visit Count</button>
+    <br><hr>
+    <h1>{{test2}}</h1>
+
+
+    <br>
+    <h1> Hello ~ </h1>
+    <br>
+
+    <h1>Tip</h1>
+    <p>V2 버전은 v-on:click="" , v-on:keyup=""</p>
+    <p>V3 버전은 @click , @keyup=""</p>
+    <p>V4 버전은? ㅜㅜ</p>
+
+
+    <br><br>
     <img alt="Vue logo" src="./assets/logo.png">
+    <br><br><br><br><br><br>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -12,7 +32,26 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      test:"test",
+      visit: 0,
+      isVisible: false
+    }
+  },
+  methods: {
+    visitCount() {
+      this.visit++;
+      this.isVisible = true;
+    }
+  },
+  computed: {
+    test2(){ // 아래 변수가 변화하는 것을 보고 있다, computed Properties !!
+      return "hello ~ ! " + this.visit;
+    }
   }
+
 }
 </script>
 
